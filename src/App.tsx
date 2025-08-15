dcefac98d0ff7e1192ec4e2137bfd4904514996a
Renan Martins <renanmartins@locem.com.br>
2025-08-15 17:45:26 -0300

    chore: setup tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import './styles/global.css';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-lime-500">
+      <Text className='text-base'>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
