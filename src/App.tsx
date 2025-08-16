6292a16bf46a6ef4e79327122daca1c6e85bbb9b
Renan Martins <renanmartins@locem.com.br>
2025-08-16 15:58:13 -0300

    chore: add expo google fonts and expo splash screen dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { HostGrotesk_400Regular, HostGrotesk_500Medium, HostGrotesk_600SemiBold, HostGrotesk_700Bold, useFonts } from '@expo-google-fonts/host-grotesk';
+import * as SplashScreen from 'expo-splash-screen';
 
 import './styles/global.css';
 
+SplashScreen
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-lime-500">
