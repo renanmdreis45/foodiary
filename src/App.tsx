a8f2edc72889134a86fe061e460cab99d622ee49
Renan Martins <renanmartins@locem.com.br>
2025-08-19 19:03:31 -0300

    Configure fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,31 @@ import { HostGrotesk_400Regular, HostGrotesk_500Medium, HostGrotesk_600SemiBold,
 import * as SplashScreen from 'expo-splash-screen';
 
 import './styles/global.css';
+import { useEffect } from 'react';
 
-SplashScreen
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+
+  if(!loaded && !error) {
+    return null;
+  }
+
+
   return (
     <View className="flex-1 items-center justify-center bg-lime-500">
       <Text className='text-base'>Open up App.tsx to start working on your app!</Text>
