19a9eb9c41f9494f5f21c11af860957da335ae66
renan martins <renanmdreis@alu.ufc.br>
2025-08-27 18:25:28 -0300

    feat: add colors const

---
+++ src/styles/colors.ts
@@ -0,0 +1,31 @@
+export const colors = {
+    black: {
+        DEFAULT: '#000000',
+        700: '#181818',
+    },
+    gray: {
+        100: '#FAFAFA',
+        200: '#F4F4F5',
+        300: '#F3F4F6',
+        400: '#E4E4E7',
+        500: '#D9D9D9',
+        600: '#A1A1A1',
+        700: '#71717A',
+    },
+    lime: {
+        400: '#E8F886',
+        500: '#BEF264',
+        600: '#A2E635',
+        700: '#64A30D',
+        800: '#1A2E05'
+    },
+    support: {
+        ambar: '#FFFF00',
+        green: '#10B981',
+        orange: '#F4A462',
+        red: '#EF4444',
+        teal: '#2A9D90',
+        tomato: '#E76E50',
+        yellow: '#E8C468',
+    }
+};
