b49bd65fbbc225638712df4bd2f6de95ca84e58c
Renan Martins <renanmdreis@alu.ufc.br>
2025-08-30 19:22:13 -0300

    feat: finish bottom tab

--- src/components/CreateMealBottomBar.tsx
+++ src/components/CreateMealBottomBar.tsx
@@ -1,15 +1,27 @@
 import { View, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Button } from './Button';
+import { CameraIcon, MicIcon } from 'lucide-react-native';
 
 export function CreateMealBottomBar() {
   const { bottom } = useSafeAreaInsets();
 
   return (
     <View
-      className="absolute bg-white z-10 w-full bottom-0 border-t border-gray-400"
+      className="absolute bg-white z-10 w-full bottom-0 border-t border-gray-400 justify-center gap-4"
       style={{ height: 80 + bottom }}
     >
-      <Text>Criar</Text>
+      <View className="flex-row mx-auto gap-4 mt-4">
+        <Text>
+          <Button size="icon" color="gray">
+            <MicIcon />
+          </Button>
+
+          <Button size="icon" color="gray">
+            <CameraIcon />
+          </Button>
+        </Text>
+      </View>
     </View>
   );
 }
