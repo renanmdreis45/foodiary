09a1da600938e1c6a49a59ed5bf20f7b653fe419
Renan Martins <renanmdreis@alu.ufc.br>
2025-08-29 23:27:34 -0300

    feat: add daily stats component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { HostGrotesk_400Regular, HostGrotesk_500Medium, HostGrotesk_600SemiBold, HostGrotesk_700Bold, useFonts } from '@expo-google-fonts/host-grotesk';
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+  useFonts,
+} from '@expo-google-fonts/host-grotesk';
 import * as SplashScreen from 'expo-splash-screen';
 
 import './styles/global.css';
 import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { HomeHeader } from './components/HomeHeader';
+import { DateSwitcher } from './components/DateSwitcher';
+import { DailyStats } from './components/DailyStats';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-
   const [loaded, error] = useFonts({
     HostGrotesk_400Regular,
     HostGrotesk_500Medium,
@@ -25,18 +32,34 @@ export default function App() {
     }
   }, [loaded, error]);
 
-
-  if(!loaded && !error) {
+  if (!loaded && !error) {
     return null;
   }
-
 
   return (
     <View className="flex-1 bg-white">
       <SafeAreaProvider>
         <HomeHeader />
+        <DateSwitcher />
+        <DailyStats
+          calories={{
+            current: 1100,
+            goal: 2500,
+          }}
+          proteins={{
+            current: 1500,
+            goal: 2500,
+          }}
+          carbohydrates={{
+            current: 2000,
+            goal: 2500,
+          }}
+          fats={{
+            current: 600,
+            goal: 2500,
+          }}
+        />
       </SafeAreaProvider>
     </View>
   );
 }
-
