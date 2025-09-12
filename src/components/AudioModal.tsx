ecd9f2aadc0d9079adaa80dc91832068c7fcf084
renan martins <renanmdreis@alu.ufc.br>
2025-09-12 20:19:51 -0300

    feat: finish audio function

--- src/components/AudioModal.tsx
+++ src/components/AudioModal.tsx
@@ -3,13 +3,22 @@ import { useEffect, useState } from 'react';
 import { Modal, SafeAreaView, View, Text, Alert } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Button } from './Button';
-import { MicIcon, SquareIcon, XIcon } from 'lucide-react-native';
+import {
+  CheckIcon,
+  MicIcon,
+  PauseIcon,
+  PlayIcon,
+  SquareIcon,
+  Trash2Icon,
+  XIcon,
+} from 'lucide-react-native';
 import { colors } from '../styles/colors';
 import { cn } from '../utils/cn';
 import {
   AudioModule,
   RecordingPresets,
   setAudioModeAsync,
+  useAudioPlayer,
   useAudioRecorder,
   useAudioRecorderState,
 } from 'expo-audio';
@@ -26,6 +35,7 @@ export function AudioModal({ open, onClose }: IAudioModalProps) {
   const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
   const recorderState = useAudioRecorderState(audioRecorder);
   const { isRecording } = useAudioRecorderState(audioRecorder);
+  const player = useAudioPlayer(audioUri);
 
   useEffect(() => {
     (async () => {
@@ -48,25 +58,15 @@ export function AudioModal({ open, onClose }: IAudioModalProps) {
 
   async function handleStopRecording() {
     await audioRecorder.stop();
-    alert(audioRecorder.uri);
-  }
-
-  function handlePlay() {
-    setIsPlaying(true);
-  }
-
-  function handlePause() {
-    setIsPlaying(false);
+    setAudioUri(audioRecorder.uri);
   }
 
   function handleDeleteAudio() {
     setAudioUri(null);
-    setIsPlaying(false);
   }
 
   function handleCloseModal() {
     setAudioUri(null);
-    setIsPlaying(false);
     onClose();
   }
 
@@ -136,6 +136,35 @@ export function AudioModal({ open, onClose }: IAudioModalProps) {
                 <Text className="text-gray-100 text-base font-sans-regular">
                   Toque no microfone para começar a gravar
                 </Text>
+              </View>
+            )}
+
+            {audioUri && (
+              <View className="p-5 pt-6 items-center pb-20 flex-row justify-center gap-8">
+                <Button size="icon" color="dark" onPress={handleDeleteAudio}>
+                  <Trash2Icon size={20} color={colors.gray[500]}></Trash2Icon>
+                </Button>
+                {!player.playing && (
+                  <Button
+                    size="icon"
+                    color="dark"
+                    onPress={() => player.play()}
+                  >
+                    <PlayIcon size={20} color={colors.lime[600]} />
+                  </Button>
+                )}
+                {player.playing && (
+                  <Button
+                    size="icon"
+                    color="dark"
+                    onPress={() => player.play()}
+                  >
+                    <PauseIcon size={20} color={colors.lime[600]} />
+                  </Button>
+                )}
+                <Button size="icon" onPress={() => player.pause()}>
+                  <CheckIcon size={20} color={colors.black[700]} />
+                </Button>
               </View>
             )}
           </SafeAreaView>
