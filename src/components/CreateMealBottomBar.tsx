c7653bcd1d26b44d1161d6ff8f5732f01833650f
renan martins <renanmdreis@alu.ufc.br>
2025-09-13 16:12:06 -0300

    chore: add expo camera dependency

--- src/components/CreateMealBottomBar.tsx
+++ src/components/CreateMealBottomBar.tsx
@@ -4,32 +4,46 @@ import { Button } from './Button';
 import { CameraIcon, MicIcon } from 'lucide-react-native';
 import { useState } from 'react';
 import { AudioModal } from './AudioModal';
+import { CameraModal } from './CameraModal';
 
 export function CreateMealBottomBar() {
   const { bottom } = useSafeAreaInsets();
 
   const [isAudioModalOpen, setIsAudioModalOpen] = useState(false);
+  const [isPictureModalOpen, setIsPictureModalOpen] = useState(false);
 
   return (
     <View
-      className="absolute bg-white z-10 w-full bottom-0 border-t border-gray-400 justify-center gap-4"
+      className="absolute bg-white z-10 w-full bottom-0 border-t border-gray-400"
       style={{ height: 80 + bottom }}
     >
       <View className="flex-row mx-auto gap-4 mt-4">
         <Text>
-          <Button size="icon" color="gray" onPress={() => setIsAudioModalOpen(true)}>
+          <Button
+            size="icon"
+            color="gray"
+            onPress={() => setIsAudioModalOpen(true)}
+          >
             <MicIcon />
           </Button>
 
-          <Button size="icon" color="gray">
+          <Button
+            size="icon"
+            color="gray"
+            onPress={() => setIsPictureModalOpen(true)}
+          >
             <CameraIcon />
           </Button>
         </Text>
       </View>
 
-      <AudioModal 
+      <AudioModal
         open={isAudioModalOpen}
         onClose={() => setIsAudioModalOpen(false)}
+      />
+      <CameraModal
+        open={isPictureModalOpen}
+        onClose={() => setIsPictureModalOpen(false)}
       />
     </View>
   );
