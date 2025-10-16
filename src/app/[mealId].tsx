67c9472eb14b0c32ef1ebc3d62a4f6e5a74e9f90
renan martins <renanmdreis@alu.ufc.br>
2025-10-15 22:43:58 -0300

    feat: create details meals route

---
+++ src/app/[mealId].tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+export default function Page() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      Detalhes da refeição...
+    </View>
+  );
+}
--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'expo-router';
 import { TouchableOpacity, Text, View } from 'react-native';
 
 interface IMealCardProps {
@@ -5,24 +6,28 @@ interface IMealCardProps {
   name: string;
 }
 
-export function MealCard({ name }: IMealCardProps) {
+export function MealCard({ name, id }: IMealCardProps) {
   return (
-    <TouchableOpacity>
-      <Text className="text-base font-sans-regular text-gray-700">
-        Hoje, 12h25
-      </Text>
+    <Link href={`meals/${id}`}>
+      <TouchableOpacity>
+        <Text className="text-base font-sans-regular text-gray-700">
+          Hoje, 12h25
+        </Text>
 
-      <View className="mt-2 px-4 py-5 flex-row gap-3 items-center border border-gray-400 rounded-2xl">
-        <View className="size-12 bg-gray-200 rounded-full items-center justify-center">
-          <Text>🍞</Text>
+        <View className="mt-2 px-4 py-5 flex-row gap-3 items-center border border-gray-400 rounded-2xl">
+          <View className="size-12 bg-gray-200 rounded-full items-center justify-center">
+            <Text>🍞</Text>
+          </View>
+          <View>
+            <Text className="text-base font-sans-regular text-gray-700">
+              {name}
+            </Text>
+            <Text className="text-base font-sans-medium text-black-700">
+              Pão
+            </Text>
+          </View>
         </View>
-        <View>
-          <Text className="text-base font-sans-regular text-gray-700">
-            {name}
-          </Text>
-          <Text className="text-base font-sans-medium text-black-700">Pão</Text>
-        </View>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </Link>
   );
 }
