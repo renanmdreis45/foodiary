b3fa43c429eb17be5c5f92c66235850e44b1cf96
Renan Martins <renanmdreis@alu.ufc.br>
2025-11-07 21:58:24 -0300

    feat: init sign in with AuthLayout

--- src/app/(public)/signin.tsx
+++ src/app/(public)/signin.tsx
@@ -2,13 +2,16 @@ import { Input } from 'postcss';
 import React from 'react';
 import { Text, ImageBackground, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { AuthLayout } from '../../components/AuthLayout';
 
 export default function SignIn() {
   return (
-    <AuthLayout>
-      <View>
-        <Input label="password" secureTextEntry />
-      </View>
+    <AuthLayout
+      icon="👤"
+      title="Entre em sua conta"
+      subtitle="Acesse sua conta para continuar"
+    >
+      <View></View>
     </AuthLayout>
   );
 }
